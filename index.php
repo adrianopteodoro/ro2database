5e7e49521250db4b5e1f4f2c72c6f3b5899015c7
Adriano Teodoro <adrianopteodoro@gmail.com>
2018-02-10 19:03:42 -0200

    Added template engine twig

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
 <?php
-include "./include/main.inc.php";
+/**
+ * Index
+ * 
+ * PHP version 5
+ *
+ * @category Index
+ * @package  RO2Database
+ * @author   Adriano Teodoro <adrianopteodoro@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/adrianopteodoro/ro2database
+ */
+
+require "./include/main.inc.php";
+require_once './vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment(
+    $loader,
+    array(
+        'cache' => '/path/to/compilation_cache',
+    )
+);
+$template = $twig->load('index.html');
+echo $template->render(array('js' => javaLibs()));
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ragnarok 2 Database</title>
-        <meta http-equiv="Content-type" content="text/html; charset=euc-kr" />
-        <link rel="stylesheet" type="text/css" href="styles/main.css" />
-<?php javaLibs(); ?>
-        <script type="text/javascript" src="js/functions.js"></script>
-    </head>
-    <body> <!-- onload="window.getItems(0, 0, 20);" -->
-        <div id="wrapper">
-            <div id="container">
-                <div id="nav">
-                    <ul>
-						<li><a href="./">Home<a></li>
-                        <li><a href="javascript:void(0);" onClick="window.getItems();">Items List</a></li>
-                        <li><a href="javascript:void(0);">Skills List (EM BREVE)</a></li>
-                        <li><a href="javascript:void(0);">Quest List (EM BREVE)</a></li>
-                    </ul>
-                </div>
-                <div id="content">
-					<ul id="itemlist"><li id="item-0"><center><b>Selecione uma das opções acima</b></center></li></ul>
-				</div>
-                <div id="footer">
-                    Desenvolvido por Adriano Teodoro
-                </div>
-                <div style="clear: both;"></div>
-            </div>
-        </div>
-        <br style="clear: both;" />
-    </body>
-</html>
--- json.php
+++ json.php
@@ -1,34 +1,45 @@
 <?php
-include "./include/main.inc.php";
+/**
+ * JSON API
+ * 
+ * PHP version 5
+ *
+ * @category JSON_API
+ * @package  RO2Database
+ * @author   Adriano Teodoro <adrianopteodoro@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/adrianopteodoro/ro2database
+ */
+
+require "./include/main.inc.php";
 
 if (isset($_GET['get'])) {
     switch ($_GET['get']) {
-        case 'item':
-            $opt = null;
-            $start = 0;
-            $count = 5;
-            $data = [];
-            if (isset($_GET['data'])) {
-                $data = json_decode($_GET['data']);
+    case 'item':
+        $opt = null;
+        $start = 0;
+        $count = 5;
+        $data = [];
+        if (isset($_GET['data'])) {
+            $data = json_decode($_GET['data']);
+        }
+        foreach ($data as $d) {
+            if (isset($d->{'classe'})) {
+                $opt['classe'] = $d->{'classe'};
             }
-            foreach($data as $d) {
-                if (isset($d->{'classe'})) {
-                    $opt['classe'] = $d->{'classe'};
-                }
-                if (isset($d->{'category'})) {
-                    $opt['category'] = $d->{'category'};
-                }
-                if (isset($d->{'start'})) {
-                    $start = $d->{'start'};
-                }
-                if (isset($d->{'count'})) {
-                    $count = $d->{'count'};
-                }
+            if (isset($d->{'category'})) {
+                $opt['category'] = $d->{'category'};
             }
-            jsonItemData($opt, $start, $count);
-            break;
+            if (isset($d->{'start'})) {
+                $start = $d->{'start'};
+            }
+            if (isset($d->{'count'})) {
+                $count = $d->{'count'};
+            }
+        }
+        jsonItemData($opt, $start, $count);
+        break;
     }
 } else {
     die();
 }
-?>
